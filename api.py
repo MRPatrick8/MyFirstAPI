a4fc5e838e4f16588ef496b9706fae4f4c8c649f
MUYIZERE RENE PATRICK <renepatrick978@gmail.com>
2022-11-15 15:38:32 +0200

    changes requirements procfile

--- api.py
+++ api.py
@@ -1,6 +1,9 @@
 from fastapi import FastAPI, Request
 from datetime import datetime
+from easynmt import EasyNMT
+from fastapi.responses import HTMLResponse
 
+model = EasyNMT('opus-mt')
 
 storage = FastAPI(title = 'my FASTAPI')
 #flash way
@@ -13,21 +16,33 @@ def index():
 def today():
     return str(datetime.now())
 
-    
+
 @storage.get('/mynames')
 def names(First_name: bool = False, last_name: bool = False, full_name: bool = True):
     full_names = ""
     if First_name:
-        full_names += 'Gloria'
+        full_names += 'Rene'
     if last_name:
-        full_names += 'Sinseswa'
+        full_names += 'Patrick'
     if full_name:
         full_names = 'Hello my name is Gloria Sinseswa'
     return full_names
 
+@storage.get('/translation')
+def translate(text : str = ''):
+  response = model.translate([text], target_lang='fr')
+  return response[0]
 
-
-
+@storage.get('/translation-form', response_class=HTMLResponse)
+def form():
+  content =  f"""<html>
+  <form action='/translation' method='GET'>
+   <input type='text' name='text' placeholder='Please Input your Sentence'>
+   <input type='submit' value='submit'>
+  </form>
+  </html>
+  """
+  return content
 
 
 
